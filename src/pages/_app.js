0e0b97c2363631ff2290b9c6dd18d3ddf8fa83ca
Yashasewi <Yashasewisingh2668@gmail.com>
2023-03-19 02:40:13 +0530

    Added Icon and changed the favicon to the pentagon icon Added the pentagon icon to the public folder Added the title to the document page

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,15 @@
-import '@/styles/globals.css'
+import "@/styles/globals.css";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+    return (
+        <>
+            <Head>
+                <title>Pentagon</title>
+                <meta name="description" content="Pentagon" />
+                <link rel="icon" href="/Pentagon.png?ao_noptimize=1" />
+            </Head>
+            <Component {...pageProps} />
+        </>
+    );
 }
