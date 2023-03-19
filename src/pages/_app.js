309a89a0a97e03c7ebd558e0ab78a650bb7bb551
Yashasewi <32950332+Yashasewi@users.noreply.github.com>
2023-03-19 22:54:24 +0530

    added the fonts
    fonts weight are 300, 400, 500, 600, 700

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -8,6 +8,17 @@ export default function App({ Component, pageProps }) {
                 <title>Pentagon</title>
                 <meta name="description" content="Pentagon" />
                 <link rel="icon" href="/Pentagon.png?ao_noptimize=1" />
+
+                <link rel="preconnect" href="https://fonts.googleapis.com" />
+                <link
+                    rel="preconnect"
+                    href="https://fonts.gstatic.com"
+                    crossorigin
+                />
+                <link
+                    href="https://fonts.googleapis.com/css2?family=Frank+Ruhl+Libre:wght@300;400;500;600;700&display=swap"
+                    rel="stylesheet"
+                />
             </Head>
             <Component {...pageProps} />
         </>
