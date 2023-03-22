bc4e841a5f6daf159e6146ade309e8f4cb11c10a
Restyled.io <commits@restyled.io>
2023-03-22 09:15:38 +0000

    Restyled by clang-format

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,9 @@
-import { Inter } from "next/font/google";
-
-import Hero from "@/Components/Hero/Hero";
 import Collection from "@/Components/Collection/Collection";
-
-
+import Hero from "@/Components/Hero/Hero";
+import {Inter} from "next/font/google";
 
 export default function Home() {
-    return <>
-        <Hero />
-        <Collection/>
-        
-    </>;
+  return <><Hero /><Collection />
+
+         < />;
 }
