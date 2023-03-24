6c99d2ea436d52b83960920a05ac8b1d66de4009
ANANYA TRIPATHI <ananyatrip0802@gmail.com>
2023-03-24 12:09:28 +0530

    Added Review, Bestseller,

--- src/Components/Review/Review.jsx
+++ src/Components/Review/Review.jsx
@@ -1,5 +1,29 @@
 function Review() {
-  return <div>Review</div>;
+  return (
+    <div className="cardDiv">
+      <div className="card">
+        <h2 className="cardButton">T-shirt</h2>
+        <p>
+          Lorem ipsum is placeholder text commonly used in the graphic, print,
+          and publishing industries for previewing layouts and visual mockups.
+        </p>
+      </div>
+      <div className="card">
+        <h2 className="cardButton">Sweater</h2>
+        <p>
+          Lorem ipsum is placeholder text commonly used in the graphic, print,
+          and publishing industries for previewing layouts and visual mockups.
+        </p>
+      </div>
+      <div className="card">
+        <h2 className="cardButton">Long Sleeve</h2>
+        <p>
+          Lorem ipsum is placeholder text commonly used in the graphic, print,
+          and publishing industries for previewing layouts and visual mockups.
+        </p>
+      </div>
+    </div>
+  );
 }
 
 export default Review;
