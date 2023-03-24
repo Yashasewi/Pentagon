ad18a22213061500685ec97200a59b119c1b7674
Restyled.io <commits@restyled.io>
2023-03-24 21:05:23 +0000

    Restyled by prettier

--- src/Components/Collection/Collection.jsx
+++ src/Components/Collection/Collection.jsx
@@ -1,37 +1,37 @@
 import Image from "next/image";
 import card from "/public/Images/collectionCard.svg";
 function Collection() {
-    return (
-        <div className="collection">
-            <div className="collection_heading">
-                <h1>New Collection!</h1>
-                <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
-                    ullamcorper congue eros
-                </p>
-            </div>
-            <div className="cardDiv">
-                <div className="card">
-                    <Image src={card} alt="sdfgh" width={300} height={300} />
-                    <p className="cardButton">T-shirt</p>
-                </div>
-                <div className="card">
-                    <Image src={card} alt="sdfgh" width={300} height={300} />
-                    <p className="cardButton">Sweater</p>
-                </div>
-                <div className="card">
-                    <Image
-                        className="card_image "
-                        src={card}
-                        alt="sdfgh"
-                        width={300}
-                        height={300}
-                    />
-                    <p className="cardButton">Long Sleeve</p>
-                </div>
-            </div>
+  return (
+    <div className="collection">
+      <div className="collection_heading">
+        <h1>New Collection!</h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
+          ullamcorper congue eros
+        </p>
+      </div>
+      <div className="cardDiv">
+        <div className="card">
+          <Image src={card} alt="sdfgh" width={300} height={300} />
+          <p className="cardButton">T-shirt</p>
         </div>
-    );
+        <div className="card">
+          <Image src={card} alt="sdfgh" width={300} height={300} />
+          <p className="cardButton">Sweater</p>
+        </div>
+        <div className="card">
+          <Image
+            className="card_image "
+            src={card}
+            alt="sdfgh"
+            width={300}
+            height={300}
+          />
+          <p className="cardButton">Long Sleeve</p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Collection;
