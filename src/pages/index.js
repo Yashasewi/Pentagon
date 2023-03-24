4151e8dd12a7b7dd7fe0f54dae2b7d69966159f6
Restyled.io <commits@restyled.io>
2023-03-24 21:05:12 +0000

    Restyled by clang-format

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,9 @@
-import { Inter } from "next/font/google";
-
-import Hero from "@/Components/Hero/Hero";
-import Collection from "@/Components/Collection/Collection";
 import BestSeller from "@/Components/BestSeller/BestSeller";
-import Review from "@/Components/Review/Review";
+import Collection from "@/Components/Collection/Collection";
 import Footer from "@/Components/Footer/Footer";
+import Hero from "@/Components/Hero/Hero";
+import Review from "@/Components/Review/Review";
+import {Inter} from "next/font/google";
 
 export default function Home() {
   return (
