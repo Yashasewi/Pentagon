1ec9a2f6605f8c9ceb4af44c0cf5187646ad9d6f
Yashasewi <32950332+Yashasewi@users.noreply.github.com>
2023-03-25 11:07:30 +0530

    footer

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,16 +1,18 @@
 function Footer() {
     const date = new Date();
     return (
-        <div className="footer">
-            <p>Copyright @{date.getFullYear}</p>
-            <p>Men</p>
-            <p>Women</p>
-            <p>Kids</p>
-            <p>Kid</p>
-            <p>Treand</p>
-            <p>Contact Us</p>
-            <p>FAQ</p>
-        </div>
+        <>
+            <div className="footer">
+                <p>Men</p>
+                <p>Women</p>
+                <p>Kids</p>
+                <p>Kid</p>
+                <p>Treand</p>
+                <p>Contact Us</p>
+                <p>FAQ</p>
+                {/* <p>Copyright @{date.getFullYear}</p> */}
+            </div>
+        </>
     );
 }
 
