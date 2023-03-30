3cb9a3e7a753e36e3ada92301a4b077f7aaec366
Yashasewi <commits@restyled.io>
2023-03-30 04:55:52 +0000

    Restyled by prettier

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,19 +1,19 @@
 function Footer() {
-    const date = new Date();
-    return (
-        <>
-            <div className="footer">
-                <p>Men</p>
-                <p>Women</p>
-                <p>Kids</p>
-                <p>Kid</p>
-                <p>Treand</p>
-                <p>Contact Us</p>
-                <p>FAQ</p>
-                {/* <p>Copyright @{date.getFullYear}</p> */}
-            </div>
-        </>
-    );
+  const date = new Date();
+  return (
+    <>
+      <div className="footer">
+        <p>Men</p>
+        <p>Women</p>
+        <p>Kids</p>
+        <p>Kid</p>
+        <p>Treand</p>
+        <p>Contact Us</p>
+        <p>FAQ</p>
+        {/* <p>Copyright @{date.getFullYear}</p> */}
+      </div>
+    </>
+  );
 }
 
 export default Footer;
