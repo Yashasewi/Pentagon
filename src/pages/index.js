98a051ce2c2f51e424b461e2091746d15dbc1249
Restyled.io <commits@restyled.io>
2023-04-20 18:48:13 +0000

    Restyled by prettier

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,7 @@ import Collection from "@/Components/Collection/Collection";
 import Footer from "@/Components/Footer/Footer";
 import Hero from "@/Components/Hero/Hero";
 import Review from "@/Components/Review/Review";
-import {Inter} from "next/font/google";
+import { Inter } from "next/font/google";
 
 export default function Home() {
   return (
