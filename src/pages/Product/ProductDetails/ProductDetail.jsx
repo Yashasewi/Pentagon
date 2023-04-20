7ed5f9e6d141f4fbfec54000d419e910207a9f3a
Yashasewi <commits@restyled.io>
2023-04-20 18:48:13 +0000

    Restyled by prettier

--- src/pages/Product/ProductDetails/ProductDetail.jsx
+++ src/pages/Product/ProductDetails/ProductDetail.jsx
@@ -1,89 +1,88 @@
 import Image from "next/image";
 import card from "/public/Images/collectionCard.svg";
 const ProductDetails = (productData) => {
-    return (
-        <div className="productContainer">
-            <div className="productMain">
-                <div className="productImage">
-                    <Image
-                        className="active img-fluid"
-                        src={card}
-                        alt="Product Image"
-                        width={510}
-                        height={510}
-                    />
+  return (
+    <div className="productContainer">
+      <div className="productMain">
+        <div className="productImage">
+          <Image
+            className="active img-fluid"
+            src={card}
+            alt="Product Image"
+            width={510}
+            height={510}
+          />
 
-                    <div className="productImageSmall">
-                        <Image
-                            className="img-fluid"
-                            src={card}
-                            alt="Product Image"
-                            width={100}
-                            height={100}
-                        />
-                        <Image
-                            className="img-fluid"
-                            src={card}
-                            alt="Product Image"
-                            width={100}
-                            height={100}
-                        />
-                        <Image
-                            className="img-fluid"
-                            src={card}
-                            alt="Product Image"
-                            width={100}
-                            height={100}
-                        />
-                        <Image
-                            className="img-fluid"
-                            src={card}
-                            alt="Product Image"
-                            width={100}
-                            height={100}
-                        />
-                        <Image
-                            className="img-fluid"
-                            src={card}
-                            alt="Product Image"
-                            width={100}
-                            height={100}
-                        />
-                    </div>
-                </div>
-                <div className="productInfo">
-                    <h1 className="productTitle">Product Name</h1>
-                    <div className="productPrice">$50</div>
-                    <div className="productColor">
-                        <div className="colorTitle">Colour</div>
-                        <div className="colorList">
-                            <div className="colorItem">
-                                <div className="color">a</div>
-                                <div className="color">b</div>
-                                <div className="color">c</div>
-                                <div className="color">d</div>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="productSize">
-                        <div className="sizeTitle">Size</div>
-                        <div className="sizeList">
-                        
-                            <div className="sizeItem">S</div>
-                            <div className="sizeItem">M</div>
-                            <div className="sizeItem">L</div>
-                            <div className="sizeItem">XL</div>
-                            <div className="sizeItem">XXL</div>
-                        </div>
-                    </div>
-                    <div className="productButtons">
-                        <button className="addToCart">Add To Cart</button>
-                        <button className="wishList">Wishlist</button>
-                    </div>
-                </div>
-            </div>
+          <div className="productImageSmall">
+            <Image
+              className="img-fluid"
+              src={card}
+              alt="Product Image"
+              width={100}
+              height={100}
+            />
+            <Image
+              className="img-fluid"
+              src={card}
+              alt="Product Image"
+              width={100}
+              height={100}
+            />
+            <Image
+              className="img-fluid"
+              src={card}
+              alt="Product Image"
+              width={100}
+              height={100}
+            />
+            <Image
+              className="img-fluid"
+              src={card}
+              alt="Product Image"
+              width={100}
+              height={100}
+            />
+            <Image
+              className="img-fluid"
+              src={card}
+              alt="Product Image"
+              width={100}
+              height={100}
+            />
+          </div>
         </div>
-    );
+        <div className="productInfo">
+          <h1 className="productTitle">Product Name</h1>
+          <div className="productPrice">$50</div>
+          <div className="productColor">
+            <div className="colorTitle">Colour</div>
+            <div className="colorList">
+              <div className="colorItem">
+                <div className="color">a</div>
+                <div className="color">b</div>
+                <div className="color">c</div>
+                <div className="color">d</div>
+              </div>
+            </div>
+          </div>
+          <div className="productSize">
+            <div className="sizeTitle">Size</div>
+            <div className="sizeList">
+              <div className="sizeItem">S</div>
+              <div className="sizeItem">M</div>
+              <div className="sizeItem">L</div>
+              <div className="sizeItem">XL</div>
+              <div className="sizeItem">XXL</div>
+            </div>
+          </div>
+          <div className="productButtons">
+            <button className="addToCart">Add To Cart</button>
+            <button className="wishList">Wishlist</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 // export const getServersideProps = async (context) => {
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -12,16 +12,16 @@ import "../pages/Product/index.css";
 
 import Head from "next/head";
 
-export default function App({Component, pageProps}) {
-    return (
-        <>
-            <Head>
-                <title>Pentagon</title>
-                <meta name="description" content="Pentagon" />
-                <link rel="icon" href="/Pentagon.png?ao_noptimize=1" />
-            </Head>
+export default function App({ Component, pageProps }) {
+  return (
+    <>
+      <Head>
+        <title>Pentagon</title>
+        <meta name="description" content="Pentagon" />
+        <link rel="icon" href="/Pentagon.png?ao_noptimize=1" />
+      </Head>
 
-            <Component {...pageProps} />
-        </>
-    );
+      <Component {...pageProps} />
+    </>
+  );
 }
