20568505f272897d0e9b7820476be724f0c92ac9
Yashasewi <32950332+Yashasewi@users.noreply.github.com>
2023-04-24 02:20:25 +0530

    idk

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,16 +3,15 @@ import Collection from "@/Components/Collection/Collection";
 // import Footer from "@/Components/Footer/Footer";
 import Hero from "@/Components/Hero/Hero";
 import Review from "@/Components/Review/Review";
-import { Inter } from "next/font/google";
 
 export default function Home() {
-  return (
-    <>
-      <Hero />
-      <Collection />
-      <BestSeller />
-      <Review />
-      {/* <Footer /> */}
-    </>
-  );
+    return (
+        <>
+            <Hero />
+            <Collection />
+            <BestSeller />
+            <Review />
+            {/* <Footer /> */}
+        </>
+    );
 }
