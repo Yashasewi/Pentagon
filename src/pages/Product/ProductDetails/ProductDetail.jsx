ca5a112e794a081aed54468a6328e5bb71083bcf
Yashasewi <Yashasewisingh2668@gmail.com>
2023-04-25 01:15:21 +0530

    remove unwanted things

--- src/pages/Product/ProductDetails/ProductDetail.jsx
+++ src/pages/Product/ProductDetails/ProductDetail.jsx
@@ -14,220 +14,14 @@
   }
   ```
 */
-import { Fragment, useState } from "react";
+import { useState } from "react";
+import { RadioGroup } from "@headlessui/react";
 import {
-    Dialog,
-    Popover,
-    RadioGroup,
-    Tab,
-    Transition,
-} from "@headlessui/react";
-import {
-    Bars3Icon,
     CurrencyDollarIcon,
     GlobeAmericasIcon,
-    MagnifyingGlassIcon,
-    ShoppingBagIcon,
-    UserIcon,
-    XMarkIcon,
 } from "@heroicons/react/24/outline";
 import { StarIcon } from "@heroicons/react/20/solid";
 
-const navigation = {
-    categories: [
-        {
-            id: "women",
-            name: "Women",
-            featured: [
-                {
-                    name: "New Arrivals",
-                    href: "#",
-                    imageSrc:
-                        "https://tailwindui.com/img/ecommerce-images/mega-menu-category-01.jpg",
-                    imageAlt:
-                        "Models sitting back to back, wearing Basic Tee in black and bone.",
-                },
-                {
-                    name: "Basic Tees",
-                    href: "#",
-                    imageSrc:
-                        "https://tailwindui.com/img/ecommerce-images/mega-menu-category-02.jpg",
-                    imageAlt:
-                        "Close up of Basic Tee fall bundle with off-white, ochre, olive, and black tees.",
-                },
-                {
-                    name: "Accessories",
-                    href: "#",
-                    imageSrc:
-                        "https://tailwindui.com/img/ecommerce-images/mega-menu-category-03.jpg",
-                    imageAlt:
-                        "Model wearing minimalist watch with black wristband and white watch face.",
-                },
-            ],
-            sections: [
-                [
-                    {
-                        id: "shoes",
-                        name: "Shoes & Accessories",
-                        items: [
-                            { name: "Sneakers", href: "#" },
-                            { name: "Boots", href: "#" },
-                            { name: "Flats", href: "#" },
-                            { name: "Sandals", href: "#" },
-                            { name: "Heels", href: "#" },
-                            { name: "Socks", href: "#" },
-                        ],
-                    },
-                    {
-                        id: "collection",
-                        name: "Shop Collection",
-                        items: [
-                            { name: "Everything", href: "#" },
-                            { name: "Core", href: "#" },
-                            { name: "New Arrivals", href: "#" },
-                            { name: "Sale", href: "#" },
-                            { name: "Accessories", href: "#" },
-                        ],
-                    },
-                ],
-                [
-                    {
-                        id: "clothing",
-                        name: "All Clothing",
-                        items: [
-                            { name: "Basic Tees", href: "#" },
-                            { name: "Artwork Tees", href: "#" },
-                            { name: "Tops", href: "#" },
-                            { name: "Bottoms", href: "#" },
-                            { name: "Swimwear", href: "#" },
-                            { name: "Underwear", href: "#" },
-                        ],
-                    },
-                    {
-                        id: "accessories",
-                        name: "All Accessories",
-                        items: [
-                            { name: "Watches", href: "#" },
-                            { name: "Wallets", href: "#" },
-                            { name: "Bags", href: "#" },
-                            { name: "Sunglasses", href: "#" },
-                            { name: "Hats", href: "#" },
-                            { name: "Belts", href: "#" },
-                        ],
-                    },
-                ],
-                [
-                    {
-                        id: "brands",
-                        name: "Brands",
-                        items: [
-                            { name: "Full Nelson", href: "#" },
-                            { name: "My Way", href: "#" },
-                            { name: "Re-Arranged", href: "#" },
-                            { name: "Counterfeit", href: "#" },
-                            { name: "Significant Other", href: "#" },
-                        ],
-                    },
-                ],
-            ],
-        },
-        {
-            id: "men",
-            name: "Men",
-            featured: [
-                {
-                    name: "Accessories",
-                    href: "#",
-                    imageSrc:
-                        "https://tailwindui.com/img/ecommerce-images/home-page-03-category-01.jpg",
-                    imageAlt:
-                        "Wooden shelf with gray and olive drab green baseball caps, next to wooden clothes hanger with sweaters.",
-                },
-                {
-                    name: "New Arrivals",
-                    href: "#",
-                    imageSrc:
-                        "https://tailwindui.com/img/ecommerce-images/product-page-04-detail-product-shot-01.jpg",
-                    imageAlt:
-                        "Drawstring top with elastic loop closure and textured interior padding.",
-                },
-                {
-                    name: "Artwork Tees",
-                    href: "#",
-                    imageSrc:
-                        "https://tailwindui.com/img/ecommerce-images/category-page-02-image-card-06.jpg",
-                    imageAlt:
-                        "Three shirts in gray, white, and blue arranged on table with same line drawing of hands and shapes overlapping on front of shirt.",
-                },
-            ],
-            sections: [
-                [
-                    {
-                        id: "shoes",
-                        name: "Shoes & Accessories",
-                        items: [
-                            { name: "Sneakers", href: "#" },
-                            { name: "Boots", href: "#" },
-                            { name: "Sandals", href: "#" },
-                            { name: "Socks", href: "#" },
-                        ],
-                    },
-                    {
-                        id: "collection",
-                        name: "Shop Collection",
-                        items: [
-                            { name: "Everything", href: "#" },
-                            { name: "Core", href: "#" },
-                            { name: "New Arrivals", href: "#" },
-                            { name: "Sale", href: "#" },
-                        ],
-                    },
-                ],
-                [
-                    {
-                        id: "clothing",
-                        name: "All Clothing",
-                        items: [
-                            { name: "Basic Tees", href: "#" },
-                            { name: "Artwork Tees", href: "#" },
-                            { name: "Pants", href: "#" },
-                            { name: "Hoodies", href: "#" },
-                            { name: "Swimsuits", href: "#" },
-                        ],
-                    },
-                    {
-                        id: "accessories",
-                        name: "All Accessories",
-                        items: [
-                            { name: "Watches", href: "#" },
-                            { name: "Wallets", href: "#" },
-                            { name: "Bags", href: "#" },
-                            { name: "Sunglasses", href: "#" },
-                            { name: "Hats", href: "#" },
-                            { name: "Belts", href: "#" },
-                        ],
-                    },
-                ],
-                [
-                    {
-                        id: "brands",
-                        name: "Brands",
-                        items: [
-                            { name: "Re-Arranged", href: "#" },
-                            { name: "Counterfeit", href: "#" },
-                            { name: "Full Nelson", href: "#" },
-                            { name: "My Way", href: "#" },
-                        ],
-                    },
-                ],
-            ],
-        },
-    ],
-    pages: [
-        { name: "Company", href: "#" },
-        { name: "Stores", href: "#" },
-    ],
-};
 const product = {
     name: "Basic Tee",
     price: "$35",
@@ -314,6 +108,29 @@ const reviews = {
         <p>I was really pleased with the overall shopping experience. My order even included a little personal, handwritten note, which delighted me!</p>
         <p>The product quality is amazing, it looks and feel even better than I had anticipated. Brilliant stuff! I would gladly recommend this store to my friends. And, now that I think of it... I actually have, many times!</p>
       `,
+            author: "Risako M",
+            date: "May 16, 2021",
+            datetime: "2021-01-06",
+        },
+        {
+            id: 2,
+            title: "Can't say enough good things",
+            rating: 4,
+            content: `
+        <p>I was really pleased with the overall shopping experience. My order even included a little personal, handwritten note, which delighted me!</p>
+      `,
+            author: "Risako M",
+            date: "May 16, 2021",
+            datetime: "2021-01-06",
+        },
+        {
+            id: 3,
+            title: "Can't say enough good things",
+            rating: 3,
+            content: `
+        <p>I was really pleased with the overall shopping experience. My order even included a little personal, handwritten note, which delighted me!</p>
+        <p>The product quality is amazing, it looks and feel even better than I had anticipated. Brilliant stuff! I would gladly recommend this store to my friends. And, now that I think of it... I actually have, many times!</p>
+        `,
             author: "Risako M",
             date: "May 16, 2021",
             datetime: "2021-01-06",
