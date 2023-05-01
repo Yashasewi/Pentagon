2f61f7cff4105fa241dde0ad5da1ce5f16e08f02
Restyled.io <commits@restyled.io>
2023-05-01 10:03:17 +0000

    Restyled by clang-format

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,7 @@ import Collection from "@/Components/Collection/Collection";
 import Faq from "@/Components/FAQ/Faq";
 // import Footer from "@/Components/Footer/Footer";
 import Hero from "@/Components/Hero/Hero";
-import { Navbar } from "@/Components/Navbar/Navbar";
+import {Navbar} from "@/Components/Navbar/Navbar";
 import Review from "@/Components/Review/Review";
 
 export default function Home() {
