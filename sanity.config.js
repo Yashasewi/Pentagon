661e2075892f30e87eef46731ba31234064201d8
Yashasewi <32950332+Yashasewi@users.noreply.github.com>
2023-06-13 00:34:27 +0530

    added process .env

--- sanity.config.js
+++ sanity.config.js
@@ -3,14 +3,14 @@ import { deskTool } from "sanity/desk";
 import project from "./sanity/project-schema";
 
 const config = defineConfig({
-    projectId: "jw3szfyc",
-    dataset: "production",
-    title: "Befikre Backend",
-    apiVersion: "2023-04-27",
-    basePath: "/admin",
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
+  title: "Befikre Backend",
+  apiVersion: "2023-04-27",
+  basePath: "/admin",
 
-    plugins: [deskTool()],
-    schema: { types: [project] },
+  plugins: [deskTool()],
+  schema: { types: [project] },
 });
 
 export default config;
